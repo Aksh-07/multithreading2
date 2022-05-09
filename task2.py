2bef0a043749480d66e96baa935083ba566e6815
Aksh <akshdeep434@gmail.com>
2022-05-09 14:09:49 +0530

    finalising tasks

--- task2.py
+++ task2.py
@@ -18,11 +18,19 @@ def print_no(a, lock, t_id):
 
 lock_ = Lock()
 
-with concurrent.futures.ThreadPoolExecutor() as executor:
+with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
     for _ in range(4):
         id_ = get_ident()
         executor.submit(print_no, a=q, lock=lock_, t_id=id_)
 
+# threads = [Thread(target=print_no, args=(q, lock_, get_ident())) for _ in range(4)]
+#
+# for thread in threads:
+#     thread.start()
+#
+# for thread in threads:
+#     thread.join()
+#
 
 f = time.time()
 print(f"Total time: {f-s}")
