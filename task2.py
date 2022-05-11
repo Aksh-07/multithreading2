2f329d9b25396b0829697c7bfccf0d88029244c3
Aksh <akshdeep434@gmail.com>
2022-05-11 18:16:15 +0530

    working on task 2

--- task2.py
+++ task2.py
@@ -1,34 +1,42 @@
 import concurrent.futures
-from threading import get_ident, Condition, BoundedSemaphore
+from threading import get_ident, Event, BoundedSemaphore
 import time
 import queue
 
 s = time.time()
 q = queue.Queue()
-# cv = Condition()
 bs = BoundedSemaphore(4)
 
 for i in range(1, 101):
     q.put(i)
 
 
-def print_no(a):
+def print_no(a, es):
     id_ = get_ident()
     while not a.empty():
-        bs.acquire()
-        # cv.acquire()
-        # cv.notify_all()
-        print(f"Thread_{id_}: {a.get()}")
-        bs.release()
-        # cv.wait()
-        # cv.release()
+        # bs.acquire()
+        for ix, e in enumerate(es):
+            e_set = e.wait()
+            if e_set:
+                print(f"Thread_{id_}: {a.get()}")
+                e.clear()
+                if ix != len(es):
+                    es[ix+1].set()
+                else:
+                    es[0].set()
 
+        # bs.release()
+
+
+events = [Event() for _ in range(4)]
+events[0].set()
 
 with concurrent.futures.ThreadPoolExecutor() as executor:
-    threads = [executor.submit(print_no, q) for _ in range(4)]
+    # threads = [executor.submit(print_no, q, event) for event in events]
+    # for event in events:
 
+    executor.submit(print_no, q, events)
 
 f = time.time()
 
-print(f"Total time: {f-s}")
-
+print(f"Total time: {f - s}")
