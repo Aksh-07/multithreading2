086886a124bf3f7f73136399f4cb9a6e96c09d11
Aksh <akshdeep434@gmail.com>
2022-05-12 12:44:17 +0530

    working on task 2

--- task2.py
+++ task2.py
@@ -5,37 +5,40 @@ import queue
 
 s = time.time()
 q = queue.Queue()
+events_q = queue.Queue()
 bs = BoundedSemaphore(4)
 
 for i in range(1, 101):
     q.put(i)
 
 
-def print_no(a, es):
-    id_ = get_ident()
+def print_no(a, e, eq):
     while not a.empty():
-        # bs.acquire()
-        for ix, e in enumerate(es):
-            e_set = e.wait()
-            if e_set:
-                print(f"Thread_{id_}: {a.get()}")
-                e.clear()
-                if ix != len(es):
-                    es[ix+1].set()
-                else:
-                    es[0].set()
-
-        # bs.release()
+        bs.acquire()
+        id_ = get_ident()
+        e1 = eq.get()
+        e1.set()
+        e_set = e.wait()
+        if e_set:
+            print(f"Thread_{id_}: {a.get()}")
+            e1.clear()
+            eq.put(e1)
+        bs.release()
 
 
-events = [Event() for _ in range(4)]
-events[0].set()
+event1 = Event()
+event2 = Event()
+event3 = Event()
+event4 = Event()
+
+event_list = [event1, event2, event3, event4]
+for event in event_list:
+    events_q.put(event)
+
 
 with concurrent.futures.ThreadPoolExecutor() as executor:
-    # threads = [executor.submit(print_no, q, event) for event in events]
-    # for event in events:
+    threads = [executor.submit(print_no, q, event, events_q) for event in event_list]
 
-    executor.submit(print_no, q, events)
 
 f = time.time()
 
