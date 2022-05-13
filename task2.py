0991c3eea1a2838a073d30e8bd958ac15873b8c3
Aksh <akshdeep434@gmail.com>
2022-05-13 17:28:24 +0530

    working on task 2

--- task2.py
+++ task2.py
@@ -1,43 +1,99 @@
-import concurrent.futures
-from threading import get_ident, Event, BoundedSemaphore
+from threading import Event, Thread
 import time
 import queue
 
 s = time.time()
 q = queue.Queue()
-events_q = queue.Queue()
-bs = BoundedSemaphore(4)
-
-for i in range(1, 101):
-    q.put(i)
-
-
-def print_no(a, e, eq):
-    while not a.empty():
-        bs.acquire()
-        id_ = get_ident()
-        e1 = eq.get()
-        e1.set()
-        e_set = e.wait()
-        if e_set:
-            print(f"Thread_{id_}: {a.get()}")
-            e1.clear()
-            eq.put(e1)
-        bs.release()
-
-
 event1 = Event()
 event2 = Event()
 event3 = Event()
 event4 = Event()
 
-event_list = [event1, event2, event3, event4]
-for event in event_list:
-    events_q.put(event)
+for i in range(1, 101):
+    q.put(i)
 
 
-with concurrent.futures.ThreadPoolExecutor() as executor:
-    threads = [executor.submit(print_no, q, event, events_q) for event in event_list]
+class PrintNo1(Thread):
+    def __init__(self, a, name):
+        Thread.__init__(self)
+        self.queue = a
+        self.name = name
+
+    def run(self):
+        event1.set()
+        while not self.queue.empty():
+            event1_set = event1.wait()
+            if event1_set:
+                print(f"Thread_{self.name}: {self.queue.get()}")
+            event1.clear()
+            event2.set()
+            # time.sleep(0.1)
+        print(f"Thread_{self.name} finished")
+
+
+class PrintNo2(Thread):
+    def __init__(self, a, name):
+        Thread.__init__(self)
+        self.queue = a
+        self.name = name
+
+    def run(self):
+        while not self.queue.empty():
+            event2_set = event2.wait()
+            if event2_set:
+                print(f"Thread_{self.name}: {self.queue.get()}")
+            event2.clear()
+            event3.set()
+            # time.sleep(0.1)
+        print(f"Thread_{self.name} finished")
+
+
+class PrintNo3(Thread):
+    def __init__(self, a, name):
+        Thread.__init__(self)
+        self.queue = a
+        self.name = name
+
+    def run(self):
+        while not self.queue.empty():
+            event3_set = event3.wait()
+            if event3_set:
+                print(f"Thread_{self.name}: {self.queue.get()}")
+            event3.clear()
+            event4.set()
+            # time.sleep(0.1)
+        print(f"Thread_{self.name} finished")
+
+
+class PrintNo4(Thread):
+    def __init__(self, a, name):
+        Thread.__init__(self)
+        self.queue = a
+        self.name = name
+
+    def run(self):
+        while not self.queue.empty():
+            event4_set = event4.wait()
+            if event4_set:
+                print(f"Thread_{self.name}: {self.queue.get()}")
+            event4.clear()
+            event1.set()
+            # time.sleep(0.1)
+        print(f"Thread_{self.name} finished")
+
+
+t1 = PrintNo1(q, "1st")
+t2 = PrintNo2(q, "2nd")
+t3 = PrintNo3(q, "3rd")
+t4 = PrintNo4(q, "4th")
+
+t_list = [t1, t2, t3, t4]
+
+for t in t_list:
+    t.start()
+
+for t in t_list:
+    t.join()
 
 
 f = time.time()
